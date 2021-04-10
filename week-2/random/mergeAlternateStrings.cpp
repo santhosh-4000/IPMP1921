991171cb2e51ff7fcd9da56c06072dcf88bacce7
santhosh-4000 <63329733+santhosh-4000@users.noreply.github.com>
2021-04-10 20:05:13 +0530

    Create mergeAlternateStrings.cpp

---
+++ week-2/random/mergeAlternateStrings.cpp
@@ -0,0 +1,19 @@
+//https://leetcode.com/problems/merge-strings-alternately/submissions/
+string mergeAlternately(string word1, string word2) {
+        
+            int i=0;
+            string ans;
+            while(i<word1.size()&&i<word2.size()){
+                ans+=word1[i];
+                ans+=word2[i];
+                i++;
+            }
+        
+        while(i<word1.size())
+            ans+=word1[i++];
+        
+        while(i<word2.size())
+            ans+=word2[i++];
+        
+        return ans;
+    }
