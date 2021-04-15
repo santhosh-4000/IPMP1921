d5e139e2493d5f53d0495dc03b9a909150be79b1
santhosh-4000 <63329733+santhosh-4000@users.noreply.github.com>
2021-04-15 16:32:39 +0530

    hashing and sorting examples done

--- week3/LinkedList/intersectionUnion2SLL.cpp
+++ week3/LinkedList/intersectionUnion2SLL.cpp
@@ -58,3 +58,123 @@ Node* findUnion(Node* head1, Node* head2)
     }
 
 //we can also use sorting with 
+Node* mergeTwoLists(Node* A, Node* B) {
+    Node* cur1=A,*cur2=B,*cur=new Node(0);
+    Node* head=cur;
+    
+    while(cur1&&cur2){
+        if(cur1->data<=cur2->data){
+            cur->next=cur1;
+            cur1=cur1->next;
+            cur=cur->next;
+        }
+        else{
+            cur->next=cur2;
+            cur2=cur2->next;
+            cur=cur->next;
+        }
+    }
+    
+    while(cur1){
+        cur->next=cur1;
+        cur1=cur1->next;
+        cur=cur->next;
+    }
+    
+    while(cur2){
+        cur->next=cur2;
+        cur2=cur2->next;
+        cur=cur->next;
+    }
+    cur->next=NULL;
+    
+    return head->next;
+}
+
+Node* middleNode(Node* head) {
+        
+        if(head==NULL)
+            return NULL;
+        
+        Node* slow=head,*fast=head;
+        while(fast->next){
+            fast=fast->next;
+            if(fast->next){
+                fast=fast->next;
+                slow=slow->next;
+            }
+        }
+        
+        return slow;
+}
+    
+Node* mergeSort(Node* head) {
+        // your code here
+        
+        if(head==NULL||head->next==NULL)
+            return head;
+        
+        Node *mid,*head2;
+        mid=middleNode(head);
+        head2=mid->next;
+        mid->next=NULL;
+            
+        Node* node1   =mergeSort(head);
+        Node* node2   =mergeSort(head2);
+        
+       head= mergeTwoLists(node1,node2);
+        
+        return head;
+}
+
+struct Node* makeUnion(struct Node* head1, struct Node* head2)
+{
+    head1=mergeSort(head1);
+    head2=mergeSort(head2);
+    
+    Node* Result= new Node(-1);
+    Node * head=Result;/**cur1=head1,*cur2=head2*/;
+    
+    while(head1&&head2){
+        if(head1->data<head2->data){
+            if(head1->data!=Result->data){
+            Result->next=new Node(head1->data);
+            Result=Result->next;
+            }
+            head1=head1->next;
+        }
+        else if(head1->data==head2->data){
+            if(head1->data!=Result->data){
+            Result->next=new Node(head1->data);
+            Result=Result->next;
+            }
+            head1=head1->next;
+            head2=head2->next;
+        }
+        else{
+            if(head2->data!=Result->data){
+            Result->next=new Node(head2->data);
+            Result=Result->next;
+            }
+            head2=head2->next;
+        }
+    }
+    
+    while(head1){
+        if(head1->data!=Result->data){
+            Result->next=new Node(head1->data);
+            Result=Result->next;
+        }
+            head1=head1->next;
+    }
+    while(head2){
+            if(head2->data!=Result->data){
+            Result->next=new Node(head2->data);
+            Result=Result->next;
+            }
+            head2=head2->next;
+    }
+    
+    return head->next;
+    
+}
