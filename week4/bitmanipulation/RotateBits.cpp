309c9552f18b1133e3da6c596f29115ac2fa8980
santhosh-4000 <63329733+santhosh-4000@users.noreply.github.com>
2021-04-19 21:11:39 +0530

    Create RotateBits.cpp

---
+++ week4/bitmanipulation/RotateBits.cpp
@@ -0,0 +1,32 @@
+//https://practice.geeksforgeeks.org/problems/rotate-bits4524/1
+ vector <int> rotate (int n, int d)
+        {
+            //code here.
+            d=d%16;
+            uint16_t k=pow(2,16)-1,tmp=n;
+            vector <int> vec;
+            k>>=d;
+            
+            k=k|tmp;
+            k>>=(16-d);
+            tmp<<=d;
+            tmp|=k;
+            
+            vec.push_back(tmp);
+            
+            tmp=n;k=pow(2,16)-1;
+            k<<=d;
+            
+            k=k|tmp;
+            k<<=(16-d);
+            tmp>>=d;
+            tmp|=k;
+            
+            //cout<<tmp;
+            vec.push_back(tmp);
+            
+            return vec;
+        }
+
+//(n<<d)&&(n>>(31-d)) - anticlockwise
+//(n>>d)&&(n<<(31-d)) -
