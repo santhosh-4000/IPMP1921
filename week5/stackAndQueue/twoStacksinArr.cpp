86d4aec80faa4c956b670e3f77b00428e6744d9c
santhosh-4000 <63329733+santhosh-4000@users.noreply.github.com>
2021-04-26 15:30:33 +0530

    Create twoStacksinArr.cpp

---
+++ week5/stackAndQueue/twoStacksinArr.cpp
@@ -0,0 +1,5 @@
+//https://practice.geeksforgeeks.org/problems/implement-two-stacks-in-an-array/1#
+
+//M1: one from forward and one from behind (efficient)
+//M2: one from one half and other in 2nd half
+//in M1 and M2 push() and pop() take O(1) time need O(n) auxillary space for storing n elements
