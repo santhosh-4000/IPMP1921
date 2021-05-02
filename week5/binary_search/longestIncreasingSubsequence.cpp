1b29ed1043d2d1b758fc3c813fc5f0c932959782
santhosh-4000 <63329733+santhosh-4000@users.noreply.github.com>
2021-05-02 19:30:49 +0530

    continue later

---
+++ week5/binary_search/longestIncreasingSubsequence.cpp
@@ -0,0 +1,5 @@
+//https://leetcode.com/problems/longest-increasing-subsequence/
+
+//brute force will be exponential complexity O(2^n) in worst case n-length of substring
+//every element can or cannot be included and only included if contributes to increasing subsequence
+//https://afteracademy.com/blog/longest-increasing-subsequence 
