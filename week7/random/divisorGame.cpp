f9a021b0dbc57ec525c5919110427b6d7ad1b6b5
santhosh-4000 <63329733+santhosh-4000@users.noreply.github.com>
2021-05-22 20:19:53 +0530

    Create divisorGame.cpp

---
+++ week7/random/divisorGame.cpp
@@ -0,0 +1,9 @@
+//https://leetcode.com/problems/divisor-game/submissions/
+//O(1)
+//person holding odd will hold odd number
+//person with odd number loses
+bool divisorGame(int n) {
+        return n%2==0;
+    }
+
+//Dp solution is also possible
