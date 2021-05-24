6764043d88862ff13045e5a344029319d1435bda
santhosh-4000 <63329733+santhosh-4000@users.noreply.github.com>
2021-05-24 22:12:52 +0530

    Update closestPairofpoints.cpp

--- week6/Divide_and_conquer/closestPairofpoints.cpp
+++ week6/Divide_and_conquer/closestPairofpoints.cpp
@@ -10,7 +10,6 @@ class Point
     int x, y;
 };
 
-//O(1)
 float distance(Point p1,Point p2){
     return (float)(sqrt((p1.x-p2.x)*(p1.x-p2.x)+(p1.y-p2.y)*(p1.y-p2.y)));
 }
@@ -23,37 +22,47 @@ bool compareY(Point p1,Point p2){
     return p1.y<=p2.y;
 }
 
-//t(n)=2*t(n/2)+o(nlogn)
-//using masters theorem, t=o(n(logn)2)
-float closestUtil(Point p[],int i, int j){
-    if(j-i<=1) return INT_MAX;
+float bruteForce(Point p[],int i, int j){
+    float mini=FLT_MAX;
+    for(int k1=i;k1<=j;k1++){
+        for(int k2=k1+1;k2<=j;k2++){
+            mini=min(mini,distance(p[k1],p[k2]));
+        }
+    }
     
-    if(j-i==2) return distance(p[i],p[i+1]);
+    return mini;
+}
+
+float closestUtil(Point p[],int i, int j){
+    
+    if(j-i<=1) return bruteForce(p,i,j);
+    
     
     int mid=(i+j)/2;
     Point mid_pt=p[mid];
     
-    float dl=closestUtil(p,i,mid);//t(n/2)
-    float dr=closestUtil(p,mid+1,j);//t(n/2)
+    float dl=closestUtil(p,i,mid);
+    float dr=closestUtil(p,mid+1,j);
     float d=min(dl,dr);
     
     vector<Point> strip;
     
-  //O(n)
-    for(int k=i;k<j;k++){
+    for(int k=i;k<=j;k++){
         if(abs(p[k].x-mid_pt.x)<d)
             strip.push_back(p[k]);
     }
     
-    sort(strip.begin(),strip.end(),compareY);//O(nlogn)
+    sort(strip.begin(),strip.end(),compareY);
     
     float mini=INT_MAX;
-  //O(n)
     for(int k=0;k<strip.size();k++){
         for(int c=1;c<=7&&k+c<strip.size();c++){
-            int dist=distance(strip[k], strip[k+c]);
-            if(dist<mini)
+            float dist=distance(strip[k], strip[k+c]);
+            if(dist<mini){
             mini=dist;
+            cout<<strip[k].x<<","<<strip[k].y<<" "<<strip[k+c].x<<","<<strip[k+c].y<<endl;
+            cout<<mini<<endl;;
+            }
         }
     }
     
@@ -61,17 +70,20 @@ float closestUtil(Point p[],int i, int j){
 }
 
 float closest(Point p[],int n){
-    sort(p,p+n,compareX);//O(nlogn)
+    sort(p,p+n,compareX);
     
-    return closestUtil(p,0,n-1);//O(n(logn)2)
+    return closestUtil(p,0,n-1);
 }
 
 int main()
 {
-    Point P[] = {{2, 3}, {12, 30}, {40, 50}, {5, 1}, {12, 10}, {3, 4}};//how
+    Point P[] = {{2, 3}, {12, 30}, {40, 50}, {5, 1}, {12, 10}, {3, 4},{10,10}};//how
     
     int n = sizeof(P) / sizeof(P[0]);
     cout << "The smallest distance is " << closest(P, n);
     return 0;
 }
+
+
+
 
