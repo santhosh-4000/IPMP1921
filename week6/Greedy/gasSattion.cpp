b2f9009a38a081555f7017b431899166fbaeeaab
santhosh-4000 <63329733+santhosh-4000@users.noreply.github.com>
2021-05-26 20:11:25 +0530

    Create gasSattion.cpp

---
+++ week6/Greedy/gasSattion.cpp
@@ -0,0 +1,19 @@
+//https://leetcode.com/problems/gas-station/submissions/
+
+//o-n time o-1 space
+int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int sum=0,total=0,res=INT_MAX;
+        
+        for(int i=0;i<gas.size();i++){
+            sum+=gas[i]-cost[i];
+            total+=gas[i]-cost[i];
+            if(sum>=0)
+                res=min(res,i);
+            else{
+                sum=0;
+                res=INT_MAX;
+            }
+        }
+        
+        return (total>=0)?res:-1;
+    }
