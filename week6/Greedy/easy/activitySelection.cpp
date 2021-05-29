07e087bcf0274210278b1a5aec63850dd56169f9
santhosh-4000 <63329733+santhosh-4000@users.noreply.github.com>
2021-05-29 18:30:36 +0530

    Update activitySelection.cpp

--- week6/Greedy/easy/activitySelection.cpp
+++ week6/Greedy/easy/activitySelection.cpp
@@ -23,3 +23,27 @@
         
         return ans;
     }
+
+//https://leetcode.com/problems/minimum-number-of-arrows-to-burst-balloons/
+
+static bool compare(vector<int>& a, vector<int>& b){
+        return a[1]<b[1];
+    }
+    
+    int findMinArrowShots(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end(), compare);
+        
+        int last=intervals[0][1];
+        int ans=0;
+        
+        for(int i=1;i<intervals.size();i++){
+            if(last>=intervals[i][0]){
+                ans++;
+            }
+            else{
+                last=intervals[i][1];
+            }
+        }
+        
+        return intervals.size()-ans;
+    }
