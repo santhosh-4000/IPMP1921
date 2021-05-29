5f0a258c36bcc4770f48ab29dfa8b14997d68670
santhosh-4000 <63329733+santhosh-4000@users.noreply.github.com>
2021-05-29 17:32:21 +0530

    Create activitySelection.cpp

---
+++ week6/Greedy/easy/activitySelection.cpp
@@ -0,0 +1,25 @@
+//https://leetcode.com/problems/non-overlapping-intervals/submissions/
+
+//O(nlogn) time const space
+ static bool compare(vector<int>& a, vector<int>& b){
+        return a[1]<b[1];
+    }
+    
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        
+        sort(intervals.begin(), intervals.end(), compare);
+        
+        int last=intervals[0][1];
+        int ans=0;
+        
+        for(int i=1;i<intervals.size();i++){
+            if(last>intervals[i][0]){
+                ans++;
+            }
+            else{
+                last=intervals[i][1];
+            }
+        }
+        
+        return ans;
+    }
