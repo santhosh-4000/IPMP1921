68d2ca1542915bf623eacb87970fbfdb71d10601
santhosh-4000 <63329733+santhosh-4000@users.noreply.github.com>
2021-05-31 14:45:08 +0530

    Update maxwidthBT.cpp

--- week7/trees/maxwidthBT.cpp
+++ week7/trees/maxwidthBT.cpp
@@ -27,3 +27,43 @@ int getMaxWidth(Node* root)
     }
 
 //https://leetcode.com/problems/maximum-width-of-binary-tree/
+
+//here,width is sort of diatance between end nodes in each level
+//o-n time and o-w space
+int widthOfBinaryTree(TreeNode* root) {
+        
+        if(root==NULL) return 0;
+        
+        queue<pair<TreeNode*,int>> q;
+        
+        q.push({root,0});
+        int maxWidth=1;
+        
+        while(!q.empty()){
+            
+            int n=q.size();
+            
+            int dec=q.back().second;
+            //cout<<dec<<endl;
+            
+            maxWidth=max(maxWidth,q.back().second
+                         -q.front().second + 1 ); 
+            while(n--){
+                TreeNode* tmp=q.front().first;
+                int idx=q.front().second;
+                
+                //cout<<idx<<" ";
+                
+                q.pop();
+                if(tmp->left)
+                q.push({tmp->left,2*idx+1-dec});
+                if(tmp->right)
+                q.push({tmp->right,2*idx+2-dec});
+            }
+            //cout<<endl;
+            
+                                        //cout<<maxWidth<<endl;
+        }
+        
+        return maxWidth;
+    }
