270125b2862333db5a55c52d7a0f1abd3b5e4a84
santhosh-4000 <63329733+santhosh-4000@users.noreply.github.com>
2021-06-03 14:37:18 +0530

    Update isFoldable.cpp

--- week7/trees-p2/isFoldable.cpp
+++ week7/trees-p2/isFoldable.cpp
@@ -23,3 +23,27 @@ bool IsFoldable(Node* root)
     
   return  IsFoldableUtil(root->left,root->right);
 }
+
+//https://leetcode.com/problems/symmetric-tree/submissions/
+
+//
+bool IsFoldableUtil(TreeNode* root1, TreeNode* root2){
+    
+    if(root1==NULL&&root2==NULL)
+    return 1;
+    
+    if(root1==NULL||root2==NULL)
+        return 0;
+    
+    return (root1->val==root2->val)&&
+        IsFoldableUtil(root1->left,root2->right)&&
+            IsFoldableUtil(root1->right,root2->left);
+    
+}
+    
+    bool isSymmetric(TreeNode* root) {
+         if(root==NULL)
+        return 1;
+    
+  return  IsFoldableUtil(root->left,root->right);
+    }
