18e80940a0da806e2430e0ffe9d421bd1b97501d
santhosh-4000 <63329733+santhosh-4000@users.noreply.github.com>
2021-06-07 18:55:01 +0530

    Update adjList.cpp

--- week-8/Graphs/adjList.cpp
+++ week-8/Graphs/adjList.cpp
@@ -1,6 +1,7 @@
 //https://practice.geeksforgeeks.org/problems/print-adjacency-list-1587115620/1
 
-//O(V+E) time
+//O(V+E) time O(V+E) space
+//to check edge presence -O(V)
 vector<vector<int>>printGraph(int V, vector<int> adj[])
 	{
 	    // Code here
