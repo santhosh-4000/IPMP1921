9cb1365e151f00e88ebd55aa05ff9a91a397940b
santhosh-4000 <63329733+santhosh-4000@users.noreply.github.com>
2021-06-07 18:34:07 +0530

    Create adjList.cpp

---
+++ week-8/Graphs/adjList.cpp
@@ -0,0 +1,21 @@
+//https://practice.geeksforgeeks.org/problems/print-adjacency-list-1587115620/1
+
+//O(V+E) time
+vector<vector<int>>printGraph(int V, vector<int> adj[])
+	{
+	    // Code here
+	    
+	    vector<vector<int>> ans;
+	    
+	    
+	    for(int i=0;i<V;i++){
+	        vector<int> tmp;
+	        tmp.push_back(i);
+	        for(auto &it: adj[i]){
+	            tmp.push_back(it);
+	        }
+	        ans.push_back(tmp);
+	    }
+	    
+	    return ans;
+	}
