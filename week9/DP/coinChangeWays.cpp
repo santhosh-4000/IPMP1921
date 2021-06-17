d7c4ce5064b5942654ae32d7adb402047dd19dd6
santhosh-4000 <63329733+santhosh-4000@users.noreply.github.com>
2021-06-17 19:37:38 +0530

    how to do memoization top down

---
+++ week9/DP/coinChangeWays.cpp
@@ -0,0 +1,22 @@
+//https://leetcode.com/problems/coin-change-2/submissions/
+
+//O(n*m) time and O(amount) space
+//tabulation
+int change(int amount, vector<int>& coins) {
+        
+        vector<int> dp(amount+1, 0);
+        dp[0]=1;
+        
+        
+        for(int i=0;i<coins.size();i++){
+            for(int j=1;j<=amount;j++){
+                if(j>=coins[i])
+                dp[j]+=dp[j-coins[i]];
+            }
+        }
+        
+        return dp[amount];
+    }
+
+//memoization
+//
