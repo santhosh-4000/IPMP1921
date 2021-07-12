86ac06de92c30dd50edbe0c0d35f82d84b568489
santhosh-4000 <63329733+santhosh-4000@users.noreply.github.com>
2021-07-12 10:41:54 +0530

    Create duplicateEle.cpp

---
+++ prac/arrays/duplicateEle.cpp
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/find-all-duplicates-in-an-array/submissions/
+
+//O(n) time O(1) space
+vector<int> findDuplicates(vector<int>& nums) {
+        
+        vector<int> ans;
+        
+        for(int i=0;i<nums.size();i++){
+            if(nums[abs(nums[i])-1]<0)
+                ans.push_back(abs(nums[i]));
+            else
+                nums[abs(nums[i])-1]*=-1;
+        }
+        return ans;
+    }
